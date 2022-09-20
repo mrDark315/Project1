eddd1665d70686aded98dac0490db6b7efd96b48
Artur <ashykhmin04@gmail.com>
2022-09-20 22:33:31 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ВВедіть своє ім'я: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привіт {name}");
+        }
+    }
+}
